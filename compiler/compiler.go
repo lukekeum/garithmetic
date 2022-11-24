24d321475bc446b8c26e4628182313448861bee8
lukekeum <lukekeum11@gmail.com>
2022-11-25 08:40:56 +0900

    feat: add compiler/compiler.go comment

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -9,12 +9,16 @@ import (
 	"github.com/lukekeum/garithmetic/store"
 )
 
+// 최초의 컴파일러 실행 함수
+// 인자로 파일 이름을 받아옴
 func Execute(fileName string) int {
 
 	fmt.Println("Compiler: Start Compiling")
 
+	// fileName의 파일 읽어오기
 	data, err := os.ReadFile(fileName)
 
+	// 읽어오는 데 오류가 있으면 오류 발생 후 프로그램 종료
 	if err != nil {
 		panic("[Error] Error occured while reading file")
 	}
@@ -22,11 +26,14 @@ func Execute(fileName string) int {
 	content := string(data[:])
 	store := []store.Token{}
 
+	// 렉서 실행
 	store = lexer.Execute(content)
 
+	// 파서 실행
 	p := parser.New(store)
 
 	p.Execute()
 
+	// 실행 종료
 	return 0
 }
